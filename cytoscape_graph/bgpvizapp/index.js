52afecc6ff26b2a3284471ea87ce106f059b801a
adrielk <theadrielkim@gmail.com>
2022-05-08 12:49:12 -0400

    cytoscape cdn fix

--- cytoscape_graph/bgpvizapp/index.js
+++ cytoscape_graph/bgpvizapp/index.js
@@ -5,7 +5,6 @@ var cytoscape = require("cytoscape");
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
