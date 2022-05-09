9b04d60fbbdd4e396392a956edd741ef2fe968a8
adrielk <theadrielkim@gmail.com>
2022-05-08 21:06:40 -0400

    stop and summary button

--- cytoscape_graph/bgpvizapp/index.js
+++ cytoscape_graph/bgpvizapp/index.js
@@ -5,14 +5,20 @@ var cytoscape = require("cytoscape");
 const app = express();
 const port = 3000;
 
-app.use('/css', express.static(path.join(__dirname, 'css')))
-app.use('/js', express.static(path.join(__dirname, 'js')))
+app.use("/css", express.static(path.join(__dirname, "css")));
+app.use("/js", express.static(path.join(__dirname, "js")));
 
-console.log(path.join(__dirname, "css"))
+console.log(path.join(__dirname, "css"));
 
-app.get("/popper.js", function(req, res) {
-  res.sendFile(__dirname + "/node_modules/cytoscape-popper/cytoscape-popper.js");
+app.get("/popper.js", function (req, res) {
+  res.sendFile(
+    __dirname + "/node_modules/cytoscape-popper/cytoscape-popper.js"
+  );
 });
+
+// app.get("/cise.js", function (res, req) {
+//   res.sendFile(__dirname + "/node_modules/cytoscape-cise/cytoscape-cise.js");
+// });
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
